a5f59ff5b16105925f2ebded2111a0d7bba3536a
taylor-green <taylor.green2804@gmail.com>
2023-02-08 18:58:35 -0500

    created password logic and set password length criteria

--- Develop/script.js
+++ Develop/script.js
@@ -7,6 +7,9 @@ var chars2 = 'abcdefghijklmnopqrstuvwxyz';
 var chasrs3 = '!@#$%^&*()_+<->:;{}';
 var chars4 = '0123456789';
 
+var charset ='';
+var password ='';
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -18,3 +21,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// password logic
+var generatePassword = function() {
+var passLength = 0;  
+}
+
+// set password length criteria 
+passLength = prompt ('Select a password length: Min 8, Max 128.');
+
+if  (passLength >= 8 || passLength <= 128) {
+  console.log(passLength);
+} else {
+  alert('Be sure your submission is between 8 and 128 characters.')
+  return'';
+}
