dfe4309a5c62ec71a7bd9143e5b552e14f079837
taylor-green <taylor.green2804@gmail.com>
2023-02-08 17:25:31 -0500

    added variables to define all character sets

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Define all characters
+var chars1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var chars2 = 'abcdefghijklmnopqrstuvwxyz';
+var chasrs3 = '!@#$%^&*()_+<->:;{}';
+var chars4 = '0123456789';
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
