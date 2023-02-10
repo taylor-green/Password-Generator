e9c1f7b1bf4412d58a0afcbfadd1db1e04f6bb96
taylor-green <taylor.green2804@gmail.com>
2023-02-10 10:38:17 -0500

    added 'include upper/lower case?' criteria

--- Develop/script.js
+++ Develop/script.js
@@ -4,11 +4,12 @@ var generateBtn = document.querySelector("#generate");
 // Define all characters
 var chars1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var chars2 = 'abcdefghijklmnopqrstuvwxyz';
-var chasrs3 = '!@#$%^&*()_+<->:;{}';
-var chars4 = '0123456789';
+var chars3 = '0123456789';
+var chars4 = '!@#$%^&*()-+';
 
 var charset ='';
 var password ='';
+
 
 // Write password to the #password input
 function writePassword() {
@@ -21,6 +22,10 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+function writePassword(){
+  document.querySelector
+
+}
 
 // password logic
 var generatePassword = function() {
@@ -34,5 +39,23 @@ if  (passLength >= 8 || passLength <= 128) {
   console.log(passLength);
 } else {
   alert('Be sure your submission is between 8 and 128 characters.')
-  return'';
+  return '';
+  
 }
+
+// include uppercase?
+var passTypeUpper = confirm('Include uppercase letters?');
+  if (passTypeUpper){
+    charset += chars1
+  }
+
+// include lowercase?
+var passTypeLower = confirm('Include lowercase letters?');
+  if (passTypeLower){
+    charset+= chars2
+  }
+
+ for (var i =0; i <= passwordLenght; i++){
+  var randomNumber = Math.floor(Math.random() * chars.length);
+  password+= chars.substring()
+ }
